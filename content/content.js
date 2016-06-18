f99dae9be3cba66ff836bf081faa0daa0913fc0c
Torng-Lin Chen <tornglinchen@Torng-Lins-MacBook-Pro.local>
2016-06-18 16:07:50 -0400

    generate random index for the cat srcs array

--- content/content.js
+++ content/content.js
@@ -11,17 +11,17 @@ function getImgSrc() {
     return images;
 }
 
+function randomIndex() {
+    return Math.floor(Math.random() * (srcs.length - 1 - 0 + 1)) + 1
+}
 var srcs = ["https://66.media.tumblr.com/7d37581c8c41072d31aa1f7a93cac5b9/tumblr_o60xo3Vg1g1u7gnm9o1_400.gif", "https://media.giphy.com/media/gXcIuJBbRi2Va/giphy.gif", "https://i.ytimg.com/vi/mW3S0u8bj58/maxresdefault.jpg", "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg", "https://charge.co/blog/content/images/2015/10/lion-cat.gif", "http://cbsnews3.cbsistatic.com/hub/i/r/2016/03/23/38e32f54-b910-4612-8852-be9e0fbdbf73/thumbnail/620x350/440a1273973991f75a0ac768f554e37c/cat-istock.jpg", "https://media.giphy.com/media/zQR7qMJ3Esh0Y/giphy.gif", "https://secure.static.tumblr.com/d021f240ac135021f0f6cf67c2c8f2c7/rq89zdl/E0cn8e2qx/tumblr_static_tumblr_static_c1w2y4zph1k4g0w88wgkk0soc_640.jpg", "http://www.catgifpage.com/gifs/318.gif", "https://lovelace-media.imgix.net/uploads/448/3e6cbca0-5ce3-0133-0bd9-0e34a4cc753d.gif?", "http://sites.psu.edu/siowfa15/wp-content/uploads/sites/29639/2015/10/cat.jpg", "http://www.catgifs.org/wp-content/uploads/2013/09/099_manycatsinthebox_cat_gifs.gif", "http://pictures.cats-paradise.net/wp-content/uploads/2014/02/funny-cat-gif.gif", "https://i.ytimg.com/vi/JalVHgKqBXs/maxresdefault.jpg", "http://s7.favim.com/orig/151230/animal-animals-cat-cats-Favim.com-3825897.gif", "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg", "https://lh3.googleusercontent.com/-FqN9wfQ_xXU/VqOio60tpXI/AAAAAAAAvdc/-J9PQM2kdLw/w506-h750/CAT-GIF-Funny-Cat-in-Monkey-costume-eating-a-banana-haha.gif", "https://media3.giphy.com/media/j2gpL7TQBHxnO/200_s.gif", "https://img1.wsimg.com/fos/sales/cwh/8/images/cats-with-hats-shop-01.jpg", "https://media2.giphy.com/media/kIkwipWRoqeUE/giphy.gif"]
 
 
 module.exports = {
-    changeSrc: function (newSrc) {
-        console.log("new", newSrc)
-        src = newSrc;
-        console.log(src)
+    changeSrc: function (newSrcs) {
+        srcs = newSrcs;
     },
     toCats: function (percentage) {
-        console.log(srcs.length)
         var images = getImgSrc();
         if (percentage === 0) return;
 
@@ -32,12 +32,9 @@ module.exports = {
         else sliceTo = images.length;
 
         var count = 0;
-        var i = 0;
         $('img').each(function () {
             if (count === sliceTo) return;
-            if (i === srcs.length) i = 0;
-            $(this).attr('src', srcs[i]);
-            i++;
+            $(this).attr('src', srcs[randomIndex()]);
             count++;
         })
     },
--- popup/controller/cat.controller.js
+++ popup/controller/cat.controller.js
@@ -1,10 +1,13 @@
 'use strict';
 var $ = require('jquery');
 
-module.exports = function ($scope) {
+module.exports = function ($scope, CatFty) {
     $scope.biru = "https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e15/11138060_1401680673483730_1311620004_n.jpg?ig_cache_key=OTU2NjUzNTY2NTk4Mzk2MzYy.2";
     $scope.grumpy = "https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/13102397_251726421845576_268830580_n.jpg?ig_cache_key=MTIzNTc2MzQwMTMxMTA4MDk5MA%3D%3D.2";
     $scope.nala = "https://scontent-lga3-1.cdninstagram.com/t51.2885-15/e35/13355458_824127977731538_700815084_n.jpg?ig_cache_key=MTI2OTk3MDE1MTM2ODU2ODE2OQ%3D%3D.2";
+    $scope.birus = CatFty.birus;
+    $scope.grumpys = CatFty.grumpys;
+    $scope.nalas = CatFty.nalas;
 
     function setScale(info) {
         $scope.catScale = info.scale;
@@ -46,9 +49,10 @@ module.exports = function ($scope) {
         });
     }
 
-    $scope.switchImg = function (src) {
+    $scope.switchImg = function (srcs) {
+        console.log(srcs)
         window.addEventListener("click", function () {
-            console.log("switch img", src)
+            console.log("switch img", srcs)
             chrome.tabs.query({
                 active: true,
                 currentWindow: true
@@ -56,7 +60,7 @@ module.exports = function ($scope) {
                 chrome.tabs.sendMessage(tabs[0].id, {
                     from: "popup",
                     subject: "switchImg",
-                    content: src
+                    content: srcs
                 }, function () {
                     console.log("switched")
                 })
